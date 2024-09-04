0148a38eca3f5641faf46de7f11d046c6b6e3c2f
Raulsr97 <salinasraul883@gmail.com>
2024-09-04 17:12:28 -0600

    consumiendo API con fetch

--- src/main.js
+++ src/main.js
@@ -1,1 +1,52 @@
-console.log('hola');
+async function getTrendingMoviesPreview () {
+    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=4b7e17cdef3393ebce38135147105a6f')
+
+    const data = await res.json()
+
+    const movies = data.results
+    
+    movies.forEach(movie => {
+
+        const trendingPreviewMovieContainer = document.querySelector('#trendingPreview .trendingPreview-movieList')
+
+        const movieContainer = document.createElement('div')
+        movieContainer.classList.add('movie-container')
+
+        const movieImg = document.createElement('img')
+        movieImg.classList.add('movie-img')
+        movieImg.setAttribute('alt', movie.title)
+        movieImg.setAttribute('src', 'https://image.tmdb.org/t/p/w300/' + movie.poster_path)
+
+        trendingPreviewMovieContainer.appendChild(movieContainer)
+        movieContainer.appendChild(movieImg)
+        
+    });
+    
+}
+
+async function getCategoriesPreview() {
+    const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=4b7e17cdef3393ebce38135147105a6f')
+    const data = await res.json()
+
+    const categories = data.genres
+
+    categories.forEach(category => {
+        const categoriesPreview = document.querySelector('#categoriesPreview .categoriesPreview-list')
+
+        const categoryContainer = document.createElement('div')
+        categoryContainer.classList.add('category-container')
+
+        const categoryTitle = document.createElement('h3')
+        categoryTitle.classList.add('category-title')
+        categoryTitle.setAttribute('id', 'id' + category.id)
+        const categoryTitleText = document.createTextNode(category.name)
+
+        categoriesPreview.appendChild(categoryContainer)
+        categoryContainer.appendChild(categoryTitle)
+        categoryTitle.appendChild(categoryTitleText)
+    })
+    
+}
+
+getTrendingMoviesPreview() 
+getCategoriesPreview()
