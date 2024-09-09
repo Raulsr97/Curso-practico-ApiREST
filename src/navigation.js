08ded792e3c3739c05217e8879316222eb9fe875
Raul Salinas <salinasraul883@gmail.com>
2024-09-09 05:10:34 -0600

    terminando el proyecto

--- src/navigation.js
+++ src/navigation.js
@@ -1,5 +1,7 @@
 searchFormBtn.addEventListener('click', () => {
-    location.hash = '#search='
+    location.hash = '#search=' + searchFormInput.value
+
+   
 })
 
 trendingBtn.addEventListener('click', () => {
@@ -7,7 +9,7 @@ trendingBtn.addEventListener('click', () => {
 })
 
 arrowBtn.addEventListener('click', () => {
-    location.hash = '#home'
+    history.back()
 })
 
 window.addEventListener('DOMContentLoaded', navigator, false)
@@ -91,6 +93,9 @@ function movieDetailsPage() {
     categoriesPreviewSection.classList.add('inactive')
     genericSection.classList.add('inactive')
     movieDetailSection.classList.remove('inactive')
+
+    const [_, movieId] = location.hash.split('=')
+    getMovieById(movieId)
 }
 
 function searchPage() {
@@ -101,13 +106,17 @@ function searchPage() {
     arrowBtn.classList.remove('inactive')
     arrowBtn.classList.remove('header-arrow--white')
     headerTitle.classList.add('inactive')
-    headerCategoryTitle.classList.remove('inactive')
+    headerCategoryTitle.classList.add('inactive')
     searchForm.classList.remove('inactive')
 
     trendingPreviewSection.classList.add('inactive')
     categoriesPreviewSection.classList.add('inactive')
     genericSection.classList.remove('inactive')
     movieDetailSection.classList.add('inactive')
+
+     // ['#search', 'busqueda']
+     const [_, query] = location.hash.split('=') 
+     getMoviesBySearch(query)
 }
 
 function trendsPage() {
@@ -125,5 +134,9 @@ function trendsPage() {
     categoriesPreviewSection.classList.add('inactive')
     genericSection.classList.remove('inactive')
     movieDetailSection.classList.add('inactive')
+
+    headerCategoryTitle.innerHTML = 'Tendencias'
+
+    getTrendingMovies()
 }
  
